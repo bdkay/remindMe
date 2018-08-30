7e110b89e2ee7c66f38401353ad5a4240bab5f1d
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-30 00:35:34 -0500

    two weeks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
   constructor(props){
@@ -24,12 +25,15 @@ class App extends Component {
   renderReminders(){
     const { reminders } = this.props;
     return (
-      <ul className="list-group col-sm-4">
+      <ul className="list-group col-sm-8">
         {
           reminders.map(reminder => {
             return (
               <li key={reminder.id} className="list-group-item list-item">
-                <div className="list-item">{reminder.text}</div>
+                <div className="list-item">
+                  <div>{reminder.text}</div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                </div>
                 <div
                   className="list-item delete-button"
                   onClick={() => this.deleteReminder(reminder.id)}>
