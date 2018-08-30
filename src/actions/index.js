cb807ac5d5b248c4f9b16eb9a2d9b91330d8e754
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-29 22:58:49 -0500

    away ye barnacles

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 import { ADD_REMINDER } from '../constants';
 
-//Action creator
-export addReminder = (text) => {
+export const addReminder = (text) => {
   const action = {
     type: ADD_REMINDER,
     text: text
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
   constructor(props){
